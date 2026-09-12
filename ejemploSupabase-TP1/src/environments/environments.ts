232594a09c6eac7d226bb42e374c94c956637f4d
EmilianoCenturion <emilianodavidcenturion@gmail.com>
2026-09-12 00:41:38 -0300

    ADD: añadido el ejemplo de login con Supabase para el TP1

---
+++ ejemploSupabase-TP1/src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://fnklgrlxrmgcjsqulqyw.supabase.co',
+    supabasePublishableKey: 'sb_publishable_hGiYWAG5xqJRDPFgzwmasw_hXPYSEb4',
+}
